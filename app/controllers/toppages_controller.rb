7b8841a06e396d0774bddb183dbdc53ce0342965
r.m <rnm0427@gmail.com>
2021-06-02 09:20:24 +0000

    toppageとuser以下ページ表示調整

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -2,6 +2,8 @@ class ToppagesController < ApplicationController
   def index
     if logged_in?
       @cordinates = Cordinate.order(id: :desc).limit(6)
+      @likes = current_user.likes.order(id: :desc).page(params[:page]).per(6)
     end
   end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,8 @@ class UsersController < ApplicationController
   before_action :require_user_logged_in, only: [:show]
   def show
     @user = User.find(params[:id])
+    @cordinates = current_user.cordinates.order(id: :desc)
+    @likes = current_user.likes.order(id: :desc).page(params[:page]).per(6)
   end
 
   def new
@@ -17,8 +19,13 @@ class UsersController < ApplicationController
     else
       flash.now[:danger] = 'ユーザーの登録に失敗しました'
       render :new
+    end
   end
-end
+  
+  def likes
+     @user = User.find(params[:id])
+     @likes = @user.likes.order(id: :desc).page(params[:page]).per(6)
+  end 
 
 private
 
